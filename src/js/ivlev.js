7789b69f277bd8eaa1498a2367fad30e17b94316
Maxim Karachyov <karachyov.m@gmail.com>
2022-07-01 15:21:38 +0300

    nav

--- src/js/ivlev.js
+++ src/js/ivlev.js
@@ -1,1 +1,17 @@
 @@include('./partials/swiper-bundle.min.js')
+
+const swiperProducts = new Swiper('.swiper-products', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 3.5,
+        }
+    }
+})
